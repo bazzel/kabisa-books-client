0458337f7b2993012cdf43cb627608bcb901d3b3
Patrick Baselier <patrick.baselier@gmail.com>
2020-10-02 10:06:39 +0200

    Add and configure tailwind

---
+++ app/tailwind.config.js
@@ -0,0 +1,13 @@
+/*global module*/
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
